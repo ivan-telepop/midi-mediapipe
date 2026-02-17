8fcf599076419dc020af924fbf0ae98f4bee9f50
ewan <ewan@ewans-MacBook-Pro.local>
2026-02-18 02:42:27 +0300

    docs: decided to add new library - mido which is better for working with MIDI | added green text to the landmarks which shows coordinates on the screen in real time

--- hand_detector.py
+++ hand_detector.py
@@ -47,6 +47,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_connection:
             result = hands.process(image)
             print(result)
             if result.multi_hand_landmarks:
+                print("DETECTED NUMBER OF HANDS:", len(result.multi_hand_landmarks), " -- ", dir(result))
                 for one in result.multi_hand_landmarks:
                     for id, lm in enumerate(one.landmark):
                         print("ID=  ", id)
@@ -55,20 +56,42 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_connection:
                         c_x, c_y = int(lm.x * w), int(lm.y * h)
                         cv2.circle(image, (c_x, c_y), 5, (240, 160, 80))
                         pointers = [4, 8, 12, 16, 20, 0]
-                        if id in pointers:
-                            """ Importing here two instance of players"""
-                            cv2.circle(image, (c_x, c_y), 18, (60, 107, 225), cv2.FILLED)
-                            a, b = c_x / 4, c_y / 2
-                            print(f"ID={id}    LM={lm}   A={a} B={b}")
-                            # cv2.rectangle(image, (c_x, c_y), (40, 40), (82, 28, 210), thickness=2)
-                            X_H, Y_W = abs(h - c_x), abs(w - c_y)
-                            dict_data = {'id_port': id,'c_x': c_x,'c_y': c_y,'height': X_H,'width': Y_W}
-                            # check size
-                            string_data = str(dict_data) + "\n"
-                            bytes_data = string_data.encode('utf-8')
-                            print(sys.getsizeof(bytes_data) , " = Bytes")
-                            # bytes_data = [id,c_x,c_y,X_H,Y_W]
-                            socket_connection.sendall(bytes_data)
+                        base_knucles = [1,5,9,13,17]
+                        # if id in pointers:
+                        #     """ Importing here two instance of players"""
+                        #     cv2.circle(image, (c_x, c_y), 18, (60, 107, 225), cv2.FILLED)
+                        #     a, b = c_x / 4, c_y / 2
+                        #     print(f"ID={id}    LM={lm}   A={a} B={b}")
+                        #     # cv2.rectangle(image, (c_x, c_y), (40, 40), (82, 28, 210), thickness=2)
+                        #     X_H, Y_W = abs(h - c_x), abs(w - c_y)
+                        #     dict_data = {'id_port': id,'c_x': c_x,'c_y': c_y,'height': X_H,'width': Y_W}
+                        #     # check size
+                        #     string_data = str(dict_data) + "\n"
+                        #     bytes_data = string_data.encode('utf-8')
+                        #     print(sys.getsizeof(bytes_data) , " = Bytes")
+                        #     # bytes_data = [id,c_x,c_y,X_H,Y_W]
+                        if id == 16:
+                             print("LANDMARK_POSITION IS : X",int(lm.x) ,"Y : ",int(lm.y))
+                             print("ID OF LANDMARK IS ",id)
+                             # FONT & DRAW TEXT PARAMS
+                             font = cv2.FONT_HERSHEY_SIMPLEX
+                             font_scale = 0.5
+                             color = (0, 189, 69) # Green color in BGR
+                             thickness = 2
+                             # formating string as 3 digits long and cutting off zeros and dots
+
+
+                    # \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \          
+                             # Just created some digit mark for understanding how to handle coords and comunicate with the MIDI
+                             # Intermediate work but it will help to do later more then i did
+                             formated_X = str(lm.x).lstrip('0.')
+                             formated_Y = str(lm.y).lstrip('0.')
+                             mark_coords = (int(formated_X[:3]), int(formated_Y[:3]))
+                             COORD_MARK = f"X: {int(formated_X[:3])} Y: {int(formated_Y[:3])}"
+                             if mark_coords:
+                                cv2.putText(image,COORD_MARK,mark_coords, font, font_scale, color, thickness)
+
+                            #socket_connection.sendall(bytes_data)
                     mpDraw.draw_landmarks(image, one, mp.solutions.hands.HAND_CONNECTIONS)
             cv2.imshow('Camera Capturing', image)
             cv2.waitKey(1)
@@ -76,6 +99,20 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_connection:
                 break
 
 
+
+
+
+# # Define the text and drawing parameters
+# text = "Index Tip"
+# font = cv2.FONT_HERSHEY_SIMPLEX
+# font_scale = 0.5
+# color = (255, 0, 0) # Blue color in BGR
+# thickness = 2
+
+# # Draw the text near the calculated coordinates (cx, cy)
+# # You may want to offset the text slightly so it doesn't overlap the landmark
+# text_coords = (cx + 10, cy)
+# cv2.putText(image, text, text_coords, font, font_scale, color, thickness)
 
 
 
--- server.py
+++ server.py
@@ -6,27 +6,60 @@ from rtmidi import *
 from rtmidi.midiconstants import NOTE_OFF, NOTE_ON
 
 
-class MidiServicer(object):
-    """ This class works with MIDI IN / OUT
+# import time
+# import rtmidi
 
+# midiout = rtmidi.MidiOut()
+# available_ports = midiout.get_ports()
+
+# if available_ports:
+#     midiout.open_port(0)
+# else:
+#     midiout.open_virtual_port("My virtual output")
+
+# with midiout:
+#     # channel 1, middle C, velocity 112
+#     note_on = [0x90, 60, 112]
+#     note_off = [0x80, 60, 0]
+#     midiout.send_message(note_on)
+#     time.sleep(0.5)
+#     midiout.send_message(note_off)
+#     time.sleep(0.1)
+
+# del midiout
+
+
+
+
+
+class MidiPool(object):
+    """ MidiPool class: 
+    singletone works with MIDI IN / OUT
     Args:
         object (_type_): _description_
     """
     def __new__(cls):
         if not hasattr(cls, 'instance'):
-            cls.instance = super(MidiServicer, cls).__new__(cls)
+            cls.instance = super(MidiPool, cls).__new__(cls)
             return cls.instance
 
     def __init__(self):
-        self.MIDIOUT = rtmidi.MidiOut()
+        self.MIDIOUT = rtmidi.MidiOut(name="My MIDI Port's Hub")
+        print(f"OPENED MIDI PORTS IS :{self.MIDIOUT.get_ports()}")
+    def __repr__(self):
+        return f"OPENED PORTS:{self.MIDIOUT.get_port_count()}"
 
-    def play_midi(self, NOTE, PORT):
-        with (self.MIDIOUT.open_port(PORT) if self.MIDIOUT.get_ports() else
-              self.MIDIOUT.open_virtual_port("My virtual output")):
-            self.MIDIOUT.send_message([NOTE_ON, NOTE, 112])
-            time.sleep(0.1)
-            self.MIDIOUT.send_message([NOTE_OFF, NOTE, 0])
-            time.sleep(0.1)
+    def play_midi_message(self, NOTE, PORT):
+            with (self.MIDIOUT.open_port(PORT) if self.MIDIOUT.get_ports() else self.MIDIOUT.open_virtual_port("My virtual output")) as OPENED_MIDI_PORTS:
+                if OPENED_MIDI_PORTS:
+                    
+                    OPENED_MIDI_PORTS.send_message([NOTE_ON, NOTE, 112])
+                    time.sleep(0.1)
+                    OPENED_MIDI_PORTS.send_message([NOTE_OFF, NOTE, 0])
+                    time.sleep(0.1)
+                else:
+                    print(f"MIDI PORTS OPENNING ERROR:{OPENED_MIDI_PORTS}")
+
 
 # List of Notes
 CENTER_NOTES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
@@ -34,9 +67,9 @@ CENTER_NOTES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
 
 
-midi_inst = MidiServicer()
+MIDI_POOL = MidiPool()
 if __name__ == '__main__':
-    print('MIDI Server started!')
+    print(f'MIDI POOL STARTED WITH: {MIDI_POOL.__repr__()} \n')
     HOST = '127.0.0.1'
     PORT = 65432
     
@@ -55,6 +88,6 @@ if __name__ == '__main__':
                 digits_one = re.findall('\d', data_string)
                 digits_two = re.findall('\d[0-9]', data_string)
                 digits_three = re.findall('\d[0-9][0-9]', data_string)
-                midi_inst.play_midi(int(digits_two[0]), int(digits_one[0]))
+                MIDI_POOL.play_midi_message(int(digits_two[0]), int(digits_one[0]))
 
 
