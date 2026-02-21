5fbf98412424c0ac5788226e881603abe695c79d
ewan <ewan@ewans-MacBook-Pro.local>
2026-02-21 03:14:21 +0300

    some experimental work done

--- tester.py
+++ tester.py
@@ -12,23 +12,15 @@ import mido
 import multiprocessing as multi_p
 # S O C K E T   C L I E N T
 
+import numpy as np
 
+def calculate_distance(lm1,lm2):
+    return np.linalg.norm(np.array([p1.x, p1.y, p1.z]) - np.array([p2.x, p2.y, p2.z]))
 
 
 port_names = mido.get_output_names()
 note = 60
 
-
-# for one in range(50,127):
-#     print(one)
-    
-#     msg = mido.Message('note_on', note=one)
-#     note +=1
-#     port.send(msg)
-#     msg = mido.Message('note_off', note=one)
-#     time.sleep(0.5)
-
-# port.panic()
 
 
 
@@ -51,15 +43,6 @@ def midi_message_handler(port_name: str, msg_data: int):
 
 
 
-def socket_send_data(data_object):
-    HOST = '127.0.0.1'
-    PORT = 65432 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        s.sendall(data_object)
-
-
-
 
 config = dotenv_values(".env")
 
@@ -77,10 +60,13 @@ hands = mp.solutions.hands.Hands(static_image_mode=False,
 
 mpDraw = mp.solutions.drawing_utils
 
-HOST = '127.0.0.1'
-PORT = 65432
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_connection:
-        socket_connection.connect((HOST, PORT))
+
+with mp.solutions.hands.Hands(static_image_mode=False,
+                                 max_num_hands=6,
+                                 min_tracking_confidence=0.1,
+                                 min_detection_confidence=0.5) as hands:
+
+        mpDraw = mp.solutions.drawing_utils
         while True:
             _, image = web_cam.read()
             result = hands.process(image)
@@ -90,9 +76,17 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_connection:
                 print("NO HANDS DETECTED")
                 continue
             else:
+                print(" *** DETECTED LEN:", len(result.multi_hand_landmarks))
                 print("DETECTED HANDS:", result.multi_hand_landmarks)
                 print('Handedness:', result.multi_handedness)
+                # Handedness: [classification {
+                #   index: 1
+                #   score: 0.901068747
+                #   label: "Right"
+                # }
+                
                 for one in result.multi_hand_landmarks:
+                    print(f"TYPE AND LENGHT OF RESULT: {len(result.multi_hand_landmarks)}")
                     
                     for id, lm in enumerate(one.landmark):
                         print("ID=  ", id)
@@ -100,21 +94,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_connection:
                         image_height, image_width, _ = image.shape
                         c_x, c_y = int(lm.x * image_width), int(lm.y * image_height)
                         cv2.circle(image, (c_x, c_y), 5, (240, 160, 80))
-                        pointers = [4, 8, 12, 16, 20, 0]
+                        pointers = [4, 8, 12, 16, 20]
                         base_knucles = [1,5,9,13,17]
-                        # if id in pointers:
-                        #     """ Importing here two instance of players"""
-                        #     cv2.circle(image, (c_x, c_y), 18, (60, 107, 225), cv2.FILLED)
-                        #     a, b = c_x / 4, c_y / 2
-                        #     print(f"ID={id}    LM={lm}   A={a} B={b}")
-                        #     # cv2.rectangle(image, (c_x, c_y), (40, 40), (82, 28, 210), thickness=2)
-                        #     X_H, Y_W = abs(h - c_x), abs(w - c_y)
-                        #     dict_data = {'id_port': id,'c_x': c_x,'c_y': c_y,'height': X_H,'width': Y_W}
-                        #     # check size
-                        #     string_data = str(dict_data) + "\n"
-                        #     bytes_data = string_data.encode('utf-8')
-                        #     print(sys.getsizeof(bytes_data) , " = Bytes")
-                        #     # bytes_data = [id,c_x,c_y,X_H,Y_W]
+                        #distances_between_knuckles = [calculate_distance(pointers[ind],base_knucles[ind]) for ind in range(len(pointers))]
                         if id in pointers or id in base_knucles:
                              print("LANDMARK_POSITION IS : X",int(lm.x) ,"Y : ",int(lm.y))
                              print("ID OF LANDMARK IS ",id)
@@ -123,8 +105,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_connection:
                              font_scale = 0.5
                              _color = (0, 189, 69) if id in pointers else (255,51,51) # Green or red color in BGR
                              thickness = 2
-                             # formating string as 3 digits long and cutting off zeros and dots
-
                     # \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \          
                              # Just created some digit mark for understanding how to handle coords and comunicate with the MIDI
                              # Intermediate work but it will help to do later more then i did
@@ -132,7 +112,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_connection:
                              formated_Y = str(lm.y).lstrip('-0.')
                              dot_value = lm.x - lm.y
                              formated_Z = str(lm.z).lstrip('-0.')
-
                              #mark_coords = (int(formated_X[:3]), int(formated_Y[:3]))
                              mark_coords = (c_x,c_y) if lm else (0,0)
                              COORD_MARK = str(dot_value).lstrip('-0.') #f"X: {int(formated_X[:3])} Y: {int(formated_Y[:3])}"
@@ -144,33 +123,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_connection:
                                 midi_message_handler(port_name=port_names[0],msg_data=int(COORD_MARK[1:3]))
                              except BaseException as e:
                                  print(f"EXCEPTION IS: {e}", f"Current data is:{str(COORD_MARK[1:3])}")
-                             #midi_message_handler(port_name=port_names[0],msg_data=int(COORD_MARK))
-                                 
                              if mark_coords:
                                 cv2.putText(image,COORD_MARK[1:3],mark_coords, font, font_scale, _color, thickness)
-
-                            #socket_connection.sendall(bytes_data)
                     mpDraw.draw_landmarks(image, one, mp.solutions.hands.HAND_CONNECTIONS)
             cv2.imshow('Camera Capturing', image)
             cv2.waitKey(1)
             if cv2.waitKey(1) == ord('q'):
                 break
-
-
-
-
-
-# # Define the text and drawing parameters
-# text = "Index Tip"
-# font = cv2.FONT_HERSHEY_SIMPLEX
-# font_scale = 0.5
-# color = (255, 0, 0) # Blue color in BGR
-# thickness = 2
-
-# # Draw the text near the calculated coordinates (cx, cy)
-# # You may want to offset the text slightly so it doesn't overlap the landmark
-# text_coords = (cx + 10, cy)
-# cv2.putText(image, text, text_coords, font, font_scale, color, thickness)
-
-
-
